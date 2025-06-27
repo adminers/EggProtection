80901f70394f5476291fdd91fb973fc7f8070b80
Administrator <1019358770@qq.com>
2025-06-27 15:54:25 +0800

    first;

---
+++ desktop/src/com/rondeo/pixwarsspace/Fpefxf.java
@@ -0,0 +1,16 @@
+package com.rondeo.pixwarsspace;
+
+/**
+ * @Title: Fpefxf
+ * @Description: Fpefxf
+ * @Company: www.qiaweidata.com
+ * @author: shenshilong
+ * @date: 2025-06-27
+ * @version: V1.0
+ */
+public class Fpefxf {
+
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
